596094564bb996281bdaa7535f6376d81b28924d
Arjun Kathuria <arjun.kathuria8@gmail.com>
2019-07-01 16:00:55 +0530

    adds clarity via better comments

--- Perceptron/perceptron.py
+++ Perceptron/perceptron.py
@@ -2,12 +2,38 @@ import numpy as np
 
 
 class Perceptron:
+
+    """
+    parameters:-
+    ----------
+
+    eta: float
+         learning rate (number b/w 0.0 and 1.0)
+
+    n_iter: int
+         iterations over training set.
+
+    random_state: int
+         Random number generator seed.
+    """
     def __init__(self, eta=0.01, n_iter=50, random_state=1):
         self.eta = eta
         self.n_iter = n_iter
         self.random_state = random_state
 
     def fit(self, X, Y):
+        """Fits the training data onto the model.
+
+        parameters:-
+        ----------
+
+        X : {feature matrix with training samples, array like (numpy object)},
+            shape = [n_samples, n_features]
+
+        y : {output vector with target values, array like (numpy object)}
+            shape = [n_samples]
+        """
+
         rgen = np.random.RandomState(self.random_state)
         self.w_ = rgen.normal(loc=0.0, scale=0.01, size=1+X.shape[1])
 
@@ -24,9 +50,11 @@ class Perceptron:
         return self
 
     def net_input(self, X):
+        """Calculates the net input"""
+
         return np.dot(X, self.w_[1:]) + self.w_[0]
 
     def predict(self, X):
-        '''Returns the class label after unit step'''
+        """Returns the class label after applying unit step"""
 
         return np.where(self.net_input(X) >= 0.0, 1, -1)
--- Perceptron/train_perceptron.py
+++ Perceptron/train_perceptron.py
@@ -19,22 +19,22 @@ Y = np.where(Y == 'Iris-setosa', 1, -1)
 # extract sepal length and petal length
 X = df.iloc[0:100, [0, 2]].values
 
-# plt.scatter(X[:50, 0], X[:50, 1], color='red', marker='o', label='setosa')
+plt.scatter(X[:50, 0], X[:50, 1], color='red', marker='o', label='setosa')
+plt.scatter(X[50: 100, 0], X[50:100, 1], color='blue', marker='x', label='versicolor')
 
-# plt.scatter(X[50: 100, 0], X[50:100, 1], color='blue', marker='x', label='versicolor')
+plt.xlabel('sepal length [cm]')
+plt.ylabel('petal length [cm]')
+plt.legend(loc='upper left')
 
-# plt.xlabel('sepal length [cm]')
-# plt.xlabel('petal length [cm]')
-# plt.legend(loc='upper left')
-
-# plt.show()
+plt.show()
 
 ppn = Perceptron(eta=0.1, n_iter=10)
 ppn.fit(X, Y)
 
 plt.plot(range(1, len(ppn.errors_) + 1), ppn.errors_, marker='o')
-plt.xlabel('Epochs')
-plt.ylabel('Number of update')
+plt.xlabel('Iterations')
+plt.ylabel('Number of Misclassifications')
+
 print('Errors: ', ppn.errors_)
 print('see the lovely converging graph now : )')
 
